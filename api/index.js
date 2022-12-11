1e73c525bf09f09c83911400d9bd403b90495897
santiago canal <santycanal_06@hotmail.com>
2022-12-11 16:04:57 -0500

    hola mundo

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   console.log("conectado a base de datos")
   server.listen(3001, () => {
     console.log('listening at port: 3001'); // eslint-disable-line no-console
