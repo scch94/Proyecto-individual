f4de9169d92d4ad8e8497ad9575547134644e5b3
santiago canal <santycanal_06@hotmail.com>
2022-12-13 01:21:32 -0500

    listo para deploy

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,22 +1,25 @@
+import axios from 'axios'
+
 export const getDogs =()=>dispatch=>{
     return(
-        fetch('http://localhost:3001/dogs',{
-            method: 'GET',
-            mode: 'no-cors'
-        })
-        .then(response=>response.json())
+        axios.get('http://localhost:3001/dogs')
+        .then(response=>response.data)
         .then(perros=>dispatch({type:'GET_PERROS',payload:perros}))
+        // fetch('http://localhost:3001/dogs',{
+        //     method: 'GET',
+        //     mode: 'no-cors'
+        // }).then(response=>response.json()).then(perros=>dispatch({type:'GET_PERROS',payload:perros}))
     )
 }
 
 export const getTemperaments=()=>dispatch=>{
     return(
-        fetch('http://localhost:3001/temperaments',{
-            method: 'GET',
-            mode: 'no-cors'
-        })
-        .then(response=>response.json())
+        axios.get('http://localhost:3001/temperaments')
+        .then(response=>response.data)
         .then(temperamentos=>dispatch({type:'GET_TEMPERAMENTS',payload:temperamentos}))
+        //     method: 'GET',
+        //     mode: 'no-cors'
+        // }).then(response=>response.json()).then(temperamentos=>dispatch({type:'GET_TEMPERAMENTS',payload:temperamentos}))
     )
 }
 
